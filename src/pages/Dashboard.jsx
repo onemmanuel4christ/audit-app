1afc0a4375903548262368d948e079ee158e6733
onemmanuel4christ <onemmanuel4christ@gmail.com>
2022-02-18 13:02:03 +0100

    dashborad in progress

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { NotificationsNone } from '@material-ui/icons';
 
 const Wrapper = styled.div`
 
@@ -26,14 +27,48 @@ const Title = styled.h1`
     color: #05A850;
 `
 const RightSide = styled.div`
+display: flex;
+align-items: center;
 `
 const Notification = styled.div`
-    position: relative;    
+    position: relative;  
+    width: 20px;
+    height: 18px;  
+    margin-right: 25px;
 `
-const Badge = styled.div`
+const Badge = styled.span`
     position: absolute;
     top: -5px;
-    right: -5px;
+    right: -1px;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: #BC1B1B;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #FFFF;
+    font-size: 10px;
+    font-weight: 700;
+`
+const UserImageHolder = styled.div`
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color:rgba(5, 168, 80, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
+`
+const UserAvater = styled.img`
+
+`
+const Greetings = styled.span`
+    font-size: 18px;
+    color: #0F0F0F;
+    font-weight: 400;
+    margin-right: 10px;
 `
 
 const MainBody = styled.div`
@@ -116,11 +151,23 @@ const Dashboard = () => {
               </Center>
               <RightSide>
                   <Notification>
-                    N
+                      <NotificationsNone />
                       <Badge>
                       2
                       </Badge>
                   </Notification>
+                  <UserImageHolder>
+                      <UserAvater src="./images/avater.svg" alt="avater" />
+                  </UserImageHolder>
+                  <Greetings>Good Morning!</Greetings> 
+                  <span style={{
+                      cursor: 'pointer',
+                      
+                  }}>
+                      <svg width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H10L5 5L0 0Z" fill="#0F0F0F" />
+                      </svg>
+                </span>
               </RightSide>
           </HeaderTop>
           <MainBody>
