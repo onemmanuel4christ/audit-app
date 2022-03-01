9f67cc0933e98f858402575d8c731f8f5f0e4442
onemmanuel4christ <onemmanuel4christ@gmail.com>
2022-03-01 12:28:06 +0100

    table completed

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -24,7 +24,7 @@ export default function BasicTable() {
         gotoPage,
         pageCount,
         prepareRow,
-        selectedFlatRows,
+        // selectedFlatRows,
         state,
         setGlobalFilter,
     } = useTable({ 
